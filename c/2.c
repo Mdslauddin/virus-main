844c1c2fdc1b5b8da4317c25c06776de72dfdb81
Mdslauddin <mdslauddin285@gamil.com>
2023-02-10 19:16:31 +0530

    2.c shutdown

---
+++ c/2.c
@@ -0,0 +1,13 @@
+/*
+os - window 
+working - shutdown
+date - 10-02-2023
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+    system("C:\\Windows\\System32\\shutdown /s /t 0");
+    return 0;
+}
