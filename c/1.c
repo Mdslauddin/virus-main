79ecdab3168fc3c8e495c08c53ef62b868f15062
Mdslauddin <mdslauddin285@gamil.com>
2023-02-10 18:42:15 +0530

    first commit

---
+++ c/1.c
@@ -0,0 +1,17 @@
+/*
+os - ubuntu
+working - window open again and again 
+type - virus
+date - 10-02-2023
+
+
+*/
+#include<stdlib.h>
+void main()
+{
+  int i,n=5;
+  for (i=0;i<n;i++)
+  {
+    system("gedit");
+  }
+}
